233583b974c8ca2138c791cbd2e0eae4ef74f15f
Matheus Lima <matheusldasilva20088@gmail.com>
2021-05-21 19:29:29 -0300

    inicia projeto hackaton cidade empreendedora

---
+++ app/src/main/java/com/rightside/hackaton/view/activities/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.rightside.hackaton.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rightside.hackaton.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
